f069812d1882a8f811db32828d7ce57f451757d7
EvansOdhams <evansodhams@gmail.com>
2023-09-10 21:52:36 +0300

    Static Deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,89 @@
 #!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static.
+# shellcheck disable=SC2230
 
-# Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
+if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
+    apt-get update
+    apt-get -y install nginx
 fi
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+# Create config file
+SERVER_CONFIG="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
 
-# Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
 
-# Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
 
-# Give ownership of the /data/ folder to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
 
-# Update Nginx configuration to serve the content of /data/web_static/current/
-# to hbnb_static using alias
-config_content=$(cat <<EOF
-location /hbnb_static {
-    alias /data/web_static/current/;
-}
-EOF
-)
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
 
-# Remove the default symbolic link created by Nginx
-sudo rm -f /etc/nginx/sites-enabled/default
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
 
-# Create a new Nginx configuration file for the hbnb_static location
-echo "$config_content" | sudo tee /etc/nginx/sites-available/hbnb_static
+# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+HOME_PAGE="<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Home - AirBnB Clone</title>
+	</head>
+	<body>
+		<h1>Welcome to AirBnB!</h1>
+	<body>
+</html>
+"
 
-# Create a symbolic link to enable the new configuration
-sudo ln -sf /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+mkdir -p /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
 
-# Restart Nginx to apply changes
-sudo service nginx restart
+# Create the folder /data/ if it doesn’t already exist
+# Create the folder /data/web_static/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+mkdir -p /data/web_static/releases/test/
+
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+mkdir -p /data/web_static/shared/
+
+# Echo fake HTML here
+echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
+[ -d /data/web_static/current ] && rm -rf /data/web_static/current
+
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/
+# If the symbolic link already exists, it should be deleted and recreated every time the script is ran
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
+# This should be recursive; everything inside should be created/owned by this user/group.
+chown -hR diplomas:diplomas /data
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+# (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
+# Use alias inside your Nginx configuration
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+	service nginx start
+else
+	service nginx restart
+    service nginx reload
+fi
